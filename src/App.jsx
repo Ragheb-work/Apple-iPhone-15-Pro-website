49d325fbf207b978fa11a4546b9b612429344816
Ragheb-work <stanyt50@gmail.com>
2024-03-29 22:33:50 +0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
+import HowItWorks from "./components/HowItWorks";
+import Navbar from "./components/Navbar";
+import Model from "./components/Model";
+import * as Sentry from "@sentry/react";
+import Features from "./components/Features";
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  );
+};
+
+export default Sentry.withProfiler(App);
